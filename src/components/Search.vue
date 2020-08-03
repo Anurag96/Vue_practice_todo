f2861d3f5ddd7005372cdb88c6e89e74d8788223
anuragkumar96 <s531372@nwmissouri.edu>
2020-08-03 11:45:08 -0500

    Added search component

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-      
-  </div>
+   <div v-if="isEnableSearch">
+        <input type="text" placeholder="Filter" v-model="search" @keyup="filterData" /> 
+    </div>
 </template>
 
 <script>
 export default {
-
+ 
+    data() {
+      return{
+        search:'',
+        isEnableSearch: true
+    }
+    },
+    methods: {
+        filterData() {
+            this.$emit('search-data', this.search)
+        }
+    }
 }
 </script>
 
--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,54 +1,81 @@
 <template>
-  <div>
-
-    <br />
-    <table>
-      <thead>
-        <tr>
-          <th>Title of the Items</th>
-          <th>Action</th>
-          <th>Status</th>
-        </tr>
-      </thead>
-      <tbody v-bind:key="todo.id" v-for="todo in todos">
-        <Todo
-          :todo="todo"
-          @delete-todo="$emit('delete-todo', todo.id)"
-          v-on:edit-todo="$emit('edit-todo', todo)"
-        />
-      </tbody>
-    </table>
-  </div>
+    <div>
+        <br />
+        <Search is-enable-search="iEnable" @searchData="searchTodo"></Search>
+        <br/>
+        <table>
+            <thead>
+                <tr>
+                    <th>Title of the Items</th>
+                    <th>Action</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody v-bind:key="todo.id"
+                   v-for="todo in todos">
+                <Todo :todo="todo" 
+                @delete-todo="$emit('delete-todo', todo.id)"
+                v-on:edit-todo="$emit('edit-todo', todo)" />
+            </tbody>
+        </table>    
+    </div>
 </template>
 
 
 <script>
-import Todo from "./Todo";
+import Todo from "./Todo"
+import Search from "./Search"
 
 export default {
-  name: "Todos",
-  components: { Todo },
-  props: ["todos"]
+    name: "Todos",
+    components: {
+        Todo,
+        Search
+    },
+    props: ["todos"],
+    data() {
+        return {
+            iEnable: true,
+            allTodos: []
+        }
+    },
+    mounted:{
+      filteredData(){
+        this.allTodos = this.todos
+      }
+     
+    }
+    ,
+    methods: {
+        searchTodo(value) {
+            if (value) {
+                this.todos.filter
+            } else {
+                this.todos = this.allTodos;
+            }
+        }
+    }
 };
 </script>
 <style scoped>
 table {
-  border: 1px solid;
+    border: 1px solid;
 }
 
 td {
-  padding: 10px;
-  border: 1px solid black;
+    padding: 10px;
+    border: 1px solid black;
 }
 
 tr:nth-child(odd) {
-  background-color: Lightgreen;
+    background-color: Lightgreen;
 }
+
 tr,
 td,
 th {
-  text-align: center;
-  text-size-adjust: 0.58;
-  margin: 5px;
+    text-align: center;
+    text-size-adjust: 0.58;
+    margin: 5px;
 }
 </style>
